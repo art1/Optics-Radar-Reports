c6195bb948934a2508024b7a0d465b28132769dc
art1 <asscience22@gmail.com>
2016-04-27 19:26:37 +0200

    corrected task 3

--- appendix/code_4a.m
+++ appendix/code_4a.m
@@ -1,3 +1,11 @@
 clear all;
 clc;
 % load two of the tests
+set1 = load('TXT_20060228_test1.fca');
+set2 = load('TXT_20060228_test2.fca');
+
+% parse data files with format
+% UT Altitude SignalAmplitude SNR ??? ??? ???
+pcolor
+
+
